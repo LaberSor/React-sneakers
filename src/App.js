db813b87791bba97eae72546b4219f461823a063
LaberSor <vlad.shumkin.rc@gmail.com>
2021-06-23 23:58:31 +0700

    css #1

--- src/App.js
+++ src/App.js
@@ -3,21 +3,31 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="wrapper">
+      <header>
+        <div className="headerLeft">
+          <img width={40} height={40} src="/img/logo.png" />
+          <div className="headerInfo">
+            <h3>React-sneakers</h3>
+            <p>Магазин лучших кроссовок</p>
+          </div>
+        </div>
+
+
+        <ul className="headerRight">
+          <li>
+            <svg></svg>
+            <span>1205 рублей</span>
+          </li>
+          <li>
+            <svg></svg>
+          </li>
+        </ul>
+
       </header>
+      <div className="content">
+        <h1>Все кроссовки</h1>
+      </div>
     </div>
   );
 }
